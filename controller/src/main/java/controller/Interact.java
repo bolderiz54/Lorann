0983a7a5253a687a07d6a30f4e1b1c5a3ae20816
pcmga <pcmga@GAUTHIER-EXIA>
2018-05-30 11:21:54 +0200

    correction

--- controller/src/main/java/controller/Interact.java
+++ controller/src/main/java/controller/Interact.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Collections;
 import java.util.List;
 
 public class Interact implements KeyListener{
