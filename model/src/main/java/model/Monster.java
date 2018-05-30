d68848df98de5c9ffdd5eda3d27d3970606550a4
vince <vince@10.154.128.123>
2018-05-30 14:31:07 +0200

    Monster

---
+++ model/src/main/java/model/Monster.java
@@ -0,0 +1,37 @@
+package model;
+
+import showboard.IPawn;
+
+/**
+ * <h1> This class describe the structure of a monster <h1>
+ * 
+ * @author Vincent Linck
+ * @version 1.0
+ *
+ */
+public class Monster extends Being implements IMonster {
+
+	/**
+	 * The behavior that have the monster
+	 */
+	private IMonster behavior;
+	
+	/**
+	 * Instantiate a monster with a custom image and a custom behavior
+	 * @param imagePath
+	 * @param behaviour
+	 */
+	public Monster(String imagePath, IMonster behaviour) {
+		
+	}
+	
+	/**
+	 * It allows the monster to move regard of his behavior
+	 */
+	@Override
+	public void move(IPawn player, IModel model) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
