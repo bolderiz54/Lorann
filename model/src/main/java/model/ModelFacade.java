0842b3756cc6dba8f5d506eb737fd385e5bbc1cf
vince <vince@10.154.128.123>
2018-05-30 09:59:12 +0200

    Model.Score and update Contract.IScore

--- model/src/main/java/model/ModelFacade.java
+++ model/src/main/java/model/ModelFacade.java
@@ -8,43 +8,139 @@ import model.dao.ExampleDAO;
 /**
  * <h1>The Class ModelFacade provides a facade of the Model component.</h1>
  *
- * @author Jean-Aymeric DIET jadiet@cesi.fr
+ * @author Vincent Linck
  * @version 1.0
  */
 public final class ModelFacade implements IModel {
 
+	private int levelNumber = 1;
+	
+	private int[][] loadedLevel;
+	
+	private IEntity[][] map;
+	
+	private int width;
+	
+	private int height;
+	
+	private Score score;
+	
     /**
      * Instantiates a new model facade.
+     * 
+     * @param map's width
+     * @param map's height
      */
-    public ModelFacade() {
+    public ModelFacade(final int width, final int height) {
         super();
     }
 
-    /*
-     * (non-Javadoc)
-     * @see model.IModel#getExampleById(int)
-     */
-    @Override
-    public Example getExampleById(final int id) throws SQLException {
-        return ExampleDAO.getExampleById(id);
-    }
+	@Override
+	public int getScore() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
-    /*
-     * (non-Javadoc)
-     * @see model.IModel#getExampleByName(java.lang.String)
-     */
-    @Override
-    public Example getExampleByName(final String name) throws SQLException {
-        return ExampleDAO.getExampleByName(name);
-    }
+	@Override
+	public void addScore(int points) {
+		// TODO Auto-generated method stub
+		
+	}
 
-    /*
-     * (non-Javadoc)
-     * @see model.IModel#getAllExamples()
-     */
-    @Override
-    public List<Example> getAllExamples() throws SQLException {
-        return ExampleDAO.getAllExamples();
-    }
+	@Override
+	public void setScore(int points) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean loadLevel(int level) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void resetLevel() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void unloadLevel() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public IEntity[][] getMap() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setWidth(int width) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setHeight(int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Player getPlayer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Monster getMonster(int monsterNumber) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isSpellExist() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ISpell getSpell() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IEntity getOnMap(int x, int y) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void generateSpell() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void destroySpell() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
