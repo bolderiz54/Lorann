c3e9ba93e865b6c1783ec50ef6a9f6e9c9f1fa4c
HugoSorroldoni <38348146+HugoSorroldoni@users.noreply.github.com>
2018-05-30 13:44:00 +0200

    Spell

---
+++ contract/src/main/java/model/ISpell.java
@@ -0,0 +1,26 @@
+package model;
+
+/**
+ * The interface ISpell
+ * @author Hugo
+ *
+ */
+public interface ISpell {
+	
+	/**
+	 * this method allow the movement
+	 */
+	public void move();
+	
+	/**
+	 * this method reverse the direction of the spell
+	 */
+	public void reverse();
+	
+	/**
+	 * this method show if the spell was called by the player
+	 */
+	public boolean isRepelled();
+	
+
+}
---
+++ model/src/main/java/model/Spell.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Spell implements ISpell{
+
+private int animationStep();
+private boolean repelled();
+
+
+public Spell() {
+	
+}
+
+}
