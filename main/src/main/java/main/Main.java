a4dffaa732e6db17b56f3a84b2e3964bff4d3f53
vince <vince@10.154.128.123>
2018-06-04 15:34:34 +0200

    update

--- main/src/main/java/main/Main.java
+++ main/src/main/java/main/Main.java
@@ -1,7 +1,10 @@
 package main;
 
+import java.sql.SQLException;
+
 import controller.ControllerFacade;
 import model.ModelFacade;
+import model.dao.DAO;
 import view.ViewFacade;
 
 /**
@@ -20,11 +23,17 @@ public abstract class Main {
      * @throws InterruptedException 
      */
     public static void main(final String[] args) throws InterruptedException {
-    	final ModelFacade model = new ModelFacade(20, 12);
+    	/*final ModelFacade model = new ModelFacade(20, 12);
     	final ViewFacade view = new ViewFacade(model);
         final ControllerFacade controller = new ControllerFacade(view, model);
 
-        controller.start();
+        controller.start();*/
+    	
+    	try {
+			DAO.loadLevel(1);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
     }
 
 }
