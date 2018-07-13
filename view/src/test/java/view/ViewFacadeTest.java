67f8429f43304608093e515cb39cd4b0843bb627
Gauthier <Windows@192.168.1.175>
2018-07-14 00:06:11 +0200

    TESTS GENERATION

---
+++ view/src/test/java/view/ViewFacadeTest.java
@@ -0,0 +1,36 @@
+package view;
+
+import static org.junit.Assert.*;
+
+import java.awt.*;
+
+import org.junit.Test;
+
+import model.ModelFacade;
+
+
+
+
+public class ViewFacadeTest {
+
+	Rectangle view;
+	
+	@Test
+	public void testViewFacade() {
+		
+		final int tailleX = 480;
+		final int tailleY = 360;
+		this.view = new Rectangle(0, 0, tailleX, tailleY);
+		assertTrue(tailleX > 0);
+		assertTrue(tailleY > 0);
+	}
+
+	@Test
+	public void testGetInteract() {
+		ModelFacade model = new ModelFacade(10, 0);
+		final ViewFacade view = new ViewFacade(model);
+		assertTrue(view.getInteract() instanceof Interact);
+		
+	}
+	
+}
